d7543c0138d01f106a72a9c9a9171293adb1b4fc
ethanb123 <ethanbowman9@gmail.com>
2020-12-28 20:04:33 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import HomePage from "./views/HomePage"
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Switch>
+
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
